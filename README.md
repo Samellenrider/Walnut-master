ecd4d5ea6e535149f3ebf37d14d893c49ad937f1
Sam Ellenrieder <sam.ell@web.de>
2017-11-08 22:42:53 +0000

    style changes

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Walnut-master
 
+
+ideads:
+
+-You click on person see in pop up window graphics of data 
+
+https://uxdesign.cc/design-better-data-tables-4ecc99d23356
+
+- small icons next to top row words
--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ const CustomerRow = ({customer}) => (
     <td>{customer.title} {customer.first_name} {customer.last_name}</td>
     <td>{customer.profession}</td>
     <td>{customer.has_children ? "Yes" : "No"}</td>
-    <td>
-      {customer.dominant_traits[0].level}
-      {customer.dominant_traits[0].primary_trait},
-      {customer.dominant_traits[1].level}
-      {customer.dominant_traits[1].secondary_trait},
+    <td class="traits">
+      <span>{customer.dominant_traits[0].level}</span>
+      <span>{customer.dominant_traits[0].primary_trait},</span>
+      <span>{customer.dominant_traits[1].level}</span>
+      <span>{customer.dominant_traits[1].secondary_trait},</span>
     </td>
   </tr>
 );
@@ -24,6 +24,7 @@ class App extends Component {
         <header>
           <h1>Walnut</h1>
         </header>
+        <div id="container">
         <table>
           <thead>
             <tr id="toprow">
@@ -43,12 +44,7 @@ class App extends Component {
             </tbody>
           </table>
         </div>
-        <div>
-        <br>
-        </br>
-        <br>
-        </br>
-        </div>
+      </div>
       </div>
     );
   }
