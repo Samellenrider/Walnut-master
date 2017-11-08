c7cf00c987044454e0327f751a26b278468cfbf7
Sam Ellenrieder <sam.ell@web.de>
2017-11-08 19:54:12 +0000

    style changes

--- src/App.js
+++ src/App.js
@@ -43,6 +43,12 @@ class App extends Component {
             </tbody>
           </table>
         </div>
+        <div>
+        <br>
+        </br>
+        <br>
+        </br>
+        </div>
       </div>
     );
   }
