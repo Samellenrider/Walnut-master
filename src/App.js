35cbff8eb9084c779cd877a6435eb1b9a9d5bcc4
Sam Ellenrieder <sam.ell@web.de>
2017-11-08 11:18:05 +0000

    repo setup

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+// import { AppRegistry, ScrollView, Image, Text } from 'react';
+import ReactScrollTable from 'react-scroll-table';
+import customers from './customers.json';
+import './styles.scss';
+
+const CustomerRow = ({customer}) => (
+  <tr>
+    <td>{customer.title} {customer.first_name} {customer.last_name}</td>
+    <td>{customer.profession}</td>
+    <td>{customer.has_children ? "Y" : "N"}</td>
+    <td>
+      {customer.dominant_traits[0].level}
+      {customer.dominant_traits[0].primary_trait},
+      {customer.dominant_traits[1].level}
+      {customer.dominant_traits[1].secondary_trait},
+    </td>
+  </tr>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header>
+          <h1>Walnut</h1>
+        </header>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Profession</th>
+              <th>Children</th>
+              <th>Traits</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+            customers.map(c => <CustomerRow customer={c}/>)
+          }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
